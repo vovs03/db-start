ce12b8beddd299b49051631dddf6645f5788596d
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2021-07-12 10:19:51 +0300

    Add: lesson3 describe

---
+++ lesson3/practic3.sql
@@ -0,0 +1,2 @@
+-- Переименовать таблицу streams в training_groups.
+ALTER TABLE 'streams' RENAME TO 'training_groups';
---
+++ lesson3/task3-1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# будет ответ, что это не файл бд
+sqlite3 practic3.sql
