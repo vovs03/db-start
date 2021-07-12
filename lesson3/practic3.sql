a318ed5f62ed9968b63fd1c51d1f2b105e4b67fe
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2021-07-12 11:08:08 +0300

    Add: new column  at table training_groups done

--- lesson3/practic3.sql
+++ lesson3/practic3.sql
@@ -3,3 +3,6 @@ ALTER TABLE 'streams' RENAME TO 'training_groups';
 
 -- В таблице training_groups переименовать столбец даты начала обучения в started_at.
 ALTER TABLE training_groups RENAME COLUMN start_date TO started_at;
+
+-- В таблице training_groups добавить столбец даты завершения обучения в finished_at.
+ALTER TABLE training_groups ADD COLUMN finished_at TEXT;
--- lesson3/training_groups.sql
+++ lesson3/training_groups.sql
@@ -1,9 +1,9 @@
 -- training_groups
-CREATE TABLE IF NOT EXISTS training_groups (
+CREATE TABLE training_groups (
     stream_id INTEGER PRIMARY KEY AUTOINCREMENT,
     course_id INTEGER NOT NULL,
     stream_num INTEGER NOT NULL,
     started_at TEXT NOT NULL,
-    num_students INTEGER NOT NULL,
+    num_students INTEGER NOT NULL, finished_at TEXT,
     FOREIGN KEY (course_id) REFERENCES courses(course_id)
 );
